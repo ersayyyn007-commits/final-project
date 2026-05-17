775f3ea27bce4dc193ea80540285ea916a77f523
ersayyyn007-commits <ersayyyn007@gmail.com>
2026-05-17 20:03:40 +0500

    Update expense.py

--- expense.py
+++ expense.py
@@ -1,12 +1,14 @@
 class Expense:
-    def __init__(self, amount, category, date):
+    def __init__(self, amount: float, category: str, date: str, comment: str = ""):
         self.amount = amount
-        self.category = category
+        self.category = category.strip().capitalize()
         self.date = date
+        self.comment = comment.strip() if comment else "No comment"
 
-    def to_dict(self):
+    def to_dict(self) -> dict:
         return {
             "amount": self.amount,
             "category": self.category,
-            "date": self.date
+            "date": self.date,
+            "comment": self.comment
         }
